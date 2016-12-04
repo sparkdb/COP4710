d08bd9bae3be84e2d176a093c41ac5e8813b4059
dantemendez2013 <dantemendez2013@gmail.com>
2016-12-03 21:58:34 -0500

    Add files via upload

---
+++ SmashDB V1.3/headerFooter.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Header & Footer - SmashDB</title>
+		<meta name="description" content="Florida State University Database (COP4710) 
+			Fall 2016 group project with Dr.Zhao."> 
+		<meta name="keywords" content="FSU, Florida State University, Database, COP4710, 
+			Games, Video Games, Smash Brothers, Smash Bros, Smash Bro, Nintendo, Wii U">
+		<link rel="stylesheet" href="css/headerFooter.css">
+		<link rel="stylesheet" href="css/login.css">
+		<link rel="icon" href="assets/favicon.png">
+	</head>
+	
+	<body>
+		
+		<div class="homeHeader">
+			<!-- <a href="home.html"><img src="assets/logo.png" alt="logo" id="log"></a> -->
+			<a href="home.html" id="logo">SmashDB</a>
+			<a href="login.html" id="login">Login</a>
+			<a href="signup.html" id="signup">Sign Up</a>
+		</div> <!-- end of homeHeader -->
+		
+		<div class="homeFooter">
+			<a href="about.html">About</a>
+			<a href="contact.html">Contact</a>
+			<a href="privacy.html">Privacy</a>
+			<a href="api.html">API</a>
+			<br>
+			<div id="socialMedia">
+				<a href=""><img src="assets/1.png" alt="facebook" height="30px" width="30px"/></a>
+				<a href=""><img src="assets/2.png" alt="twitter" height="30px" width="30px"/></a>
+				<a href=""><img src="assets/3.png" alt="google plus" height="30px" width="30px"/></a>
+				<a href=""><img src="assets/4.png" alt="youtube" height="30px" width="30px"/></a>
+			</div>
+			&copy 2016 Florida State Univeristy
+		</div> <!-- end of homeFooter -->
+		
+	</body>
+</html>
