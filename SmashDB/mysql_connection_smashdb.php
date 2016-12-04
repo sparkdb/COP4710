6cf8b0671872bb56ae4f9effd39b1114a37fc472
sparkdb <xs14d@my.fsu.edu>
2016-12-03 21:50:43 -0500

    Delete mysql_connection_smashdb.php

--- SmashDB/mysql_connection_smashdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$username = "root"; 
-	$password = "dantemendez2013"; 
-	$host = "localhost"; 
-	$dbname = "smashdb";
-	
-	$connect = @mysqli_connect($host, $username, $password, $dbname) or die ("Could not connect to SmashDB! We have been NERFED... like Greninja!!!"); 
-?>
